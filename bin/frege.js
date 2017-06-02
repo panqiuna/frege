be77482c21de844c7a4e476bedeeff49bd0e2f33
tfan <princetoad@gmail.com>
2017-06-02 18:31:55 +0800

    cli added

---
+++ bin/frege.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+/**
+ * @fileoverview Main CLI that is run via the frege command.
+ * @author fanshenggang
+ */
+
+/* eslint no-console:off */
+
+const debug = (process.argv.indexOf('--debug') > -1);
+
+if (debug) {
+  require('debug').enable('frege:*');
+}
+
+// now we can safely include the other modules that use debug
+const cli = require('../lib/cli');
+
+process.once('uncaughtException', err => {
+  console.log(err.message);
+  console.log(err.stack);
+
+  process.exitCode = 1;
+});
+
+process.exitCode = cli.execute(process.argv);
